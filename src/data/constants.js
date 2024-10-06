55a0f1f7b8d448dd8441edb3c8ad67f9144aa09c
nijgururajofficial <nijgururaja12@gmail.com>
2024-10-06 10:59:29 -0500

    update resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/nijgururajofficial",
   resume:
-    "https://drive.google.com/file/d/1Zn72jcHIKTpQUBctbdTg6I81N3BdGRRi/view?usp=sharing",
+    "https://drive.google.com/file/d/1A_hOXDs0duetvbzyjC7AWGrQbM_F80L0/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/nijgururaj15/",
   phone: "+1 (xxx)xxx-xxxx",
   location: "Chicago, IL",
