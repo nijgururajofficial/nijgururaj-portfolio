1ced228df3716daff614ded554495f72ccca6136
nijgururajofficial <nijgururaja12@gmail.com>
2025-07-01 14:18:29 -0500

    Revert "add experience"
    This reverts commit f6cc88ed533d55356f608eef81d7d641bd85f3e6.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import HeroSection from "./components/HeroSection";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Experience from "./components/Experience"
 import Footer from "./components/Footer";
 import Education from "./components/Education";
 import ProjectDetails from "./components/ProjectDetails";
@@ -37,7 +36,7 @@ function App() {
           <HeroSection />
           <Wrapper>
             <Skills />
-            <Experience />
+            {/* <Experience /> */}
           </Wrapper>
           <Projects openModal={openModal} setOpenModal={setOpenModal} />
           <Wrapper>
